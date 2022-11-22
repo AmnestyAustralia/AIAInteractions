d508720a7f8bd8fbc27fd9afe29a4f138d5c9fad
Danny Smith <danny@gorcha.org>
2022-11-22 19:18:54 +1100

    Update AssemblyInfo

--- AIAInteractionsUIModel/Properties/AssemblyInfo.cs
+++ AIAInteractionsUIModel/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Resources;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AIAInteractionsUIModel")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("AIA interaction feature pack UI models")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Amnesty International Australia")]
 [assembly: AssemblyProduct("AIAInteractionsUIModel")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -32,8 +32,8 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+// [assembly: AssemblyVersion("1.0.0.0")]
+// [assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
