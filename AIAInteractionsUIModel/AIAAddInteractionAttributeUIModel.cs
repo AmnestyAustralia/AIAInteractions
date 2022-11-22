6a8664b8e648ad4a1c924251bb777dd0ad996547
Danny Smith <danny@gorcha.org>
2022-11-22 14:14:52 +1100

    Add UI model for AddInteractionAttribute

---
+++ AIAInteractionsUIModel/AIAAddInteractionAttributeUIModel.cs
@@ -0,0 +1,23 @@
+namespace AIAInteractionsUIModel
+{
+
+    public partial class AIAAddInteractionAttributeUIModel
+	{
+
+		private void AIAAddInteractionAttributeUIModel_Loaded(object sender, Blackbaud.AppFx.UIModeling.Core.LoadedEventArgs e)
+		{
+
+		}
+
+#region "Event handlers"
+
+		partial void OnCreated()
+		{
+			this.Loaded += AIAAddInteractionAttributeUIModel_Loaded;
+		}
+
+#endregion
+
+	}
+
+}
