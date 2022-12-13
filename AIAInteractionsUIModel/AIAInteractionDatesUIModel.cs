80b7a0fa4ddcff29ce07b7d9a2325a32bb27e526
Danny Smith <danny@gorcha.org>
2022-12-13 11:07:46 +1100

    Improve interaction dates UI

--- AIAInteractionsUIModel/AIAInteractionDatesUIModel.cs
+++ AIAInteractionsUIModel/AIAInteractionDatesUIModel.cs
@@ -1,12 +1,33 @@
+using System;
+using Blackbaud.AppFx.UIModeling.Core;
+
 namespace AIAInteractionsUIModel
 {
 
     public partial class AIAInteractionDatesUIModel
     {
 
-        private void AIAInteractionDatesUIModel_Loaded(object sender, Blackbaud.AppFx.UIModeling.Core.LoadedEventArgs e)
+        private void UpdateSubCategoryEnabledRequired()
+        {
+            bool flag = _interactioncategoryid.HasValue() && _interactioncategoryid.Value != Guid.Empty;
+            _interactionsubcategoryid.Enabled = flag;
+            _interactionsubcategoryid.Required = flag;
+        }
+
+        private void _interactioncategoryid_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (!this.Loading)
+            {
+                _interactionsubcategoryid.Value = Guid.Empty;
+            }
+            _interactionsubcategoryid.ResetDataSource();
+            UpdateSubCategoryEnabledRequired();
+        }
+
+        private void AIAInteractionDatesUIModel_Loaded(object sender, LoadedEventArgs e)
         {
             RECORDTYPEID.Value = Blackbaud.AppFx.Constituent.UIModel.GlobalChangeHelper.GetRecordTypeID("INTERACTION", GetRequestContext());
+            UpdateSubCategoryEnabledRequired();
         }
 
 #region "Event handlers"
@@ -14,6 +35,7 @@ namespace AIAInteractionsUIModel
         partial void OnCreated()
         {
             this.Loaded += AIAInteractionDatesUIModel_Loaded;
+            this._interactioncategoryid.ValueChanged += _interactioncategoryid_ValueChanged;
         }
 
 #endregion
